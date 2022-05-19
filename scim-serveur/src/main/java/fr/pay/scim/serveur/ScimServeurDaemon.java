33e70b8208157a184fbb4a00aee2cee0f09d7ca4
Patrice <cora.paubry@gmail.com>
2022-05-19 22:16:43 +0200

    Préparation du serveur

---
+++ scim-serveur/src/main/java/fr/pay/scim/serveur/ScimServeurDaemon.java
@@ -0,0 +1,13 @@
+package fr.pay.scim.serveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScimServeurDaemon {
+
+	public static void main(String... args) {
+		SpringApplication.run(ScimServeurDaemon.class, args);
+	}
+	
+}
